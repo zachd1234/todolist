53bdcfcf8ec8872133eb751fe689a698ceb64bb3
Zach Derhake <zachderhake@Zachs-Laptop.router92c184.com>
2024-11-01 13:28:49 -0700

    figuring this out

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("One small step towards zero human comapnies");
         System.out.println("hi");
+        System.out.println("Hiagain!");
     } 
 } 

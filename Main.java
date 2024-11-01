60f80a2e59528cf8ca5ee3a28081e87b0d4e392c
Zach Derhake <zachderhake@Zachs-Laptop.router92c184.com>
2024-11-01 13:26:22 -0700

    now we have hi

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main { 
     public static void main(String[] args) {
         System.out.println("One small step towards zero human comapnies");
+        System.out.println("hi");
     } 
 } 
